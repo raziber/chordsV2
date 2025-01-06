c73edfe3370d9b4841c269178801b5389b8f20e2
razi berg <razi.berg@gmail.com>
2025-01-06 22:34:10 +0200

    new version created manually

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,24 +3,25 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
+import { primitiveColors } from "./PrimitiveColors";
 
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
+    text: primitiveColors.neutral600,
+    background: primitiveColors.neutral10,
+    backgroundOverlay: primitiveColors.neutral500,
+    tint: primitiveColors.neutral0,
+    icon: primitiveColors.neutral50,
+    tabIconInactive: primitiveColors.neutral40,
+    tabIconActive: primitiveColors.neutral100,
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
+    text: primitiveColors.neutral30,
+    background: primitiveColors.neutral400,
+    backgroundOverlay: primitiveColors.neutral300,
+    tint: primitiveColors.neutral80,
+    icon: primitiveColors.neutral50,
+    tabIconInactive: primitiveColors.neutral60,
+    tabIconActive: primitiveColors.neutral10,
   },
 };
