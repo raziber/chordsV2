59a11afdd3595912f8aa3a0f9d90c8ea34471fbd
razi berg <razi.berg@gmail.com>
2025-01-26 23:18:47 +0200

    fixed chords jumping around at the start

--- components/LyricsLine.tsx
+++ components/LyricsLine.tsx
@@ -1,77 +1,52 @@
-import React, { useState } from "react";
-import { View, StyleSheet, LayoutChangeEvent } from "react-native";
+import React from "react";
+import { View, StyleSheet } from "react-native";
 import { ThemedText } from "./ThemedText";
 import { ChordBox } from "./ChordBox";
 
-type ChordPosition = {
-  chord: string; // Changed from Chord type to string
-  position: number;
-};
-
-type LyricsLineProps = {
+type Props = {
   lyrics: string;
-  chords: ChordPosition[];
+  chords: Array<{
+    chord: string;
+    position: number;
+  }>;
 };
 
-export function LyricsLine({ lyrics, chords }: LyricsLineProps) {
-  const [lineWidth, setLineWidth] = useState(0);
-
-  const handleLayout = (event: LayoutChangeEvent) => {
-    setLineWidth(event.nativeEvent.layout.width);
-  };
-
-  const getXPosition = (position: number): number => {
-    if (!lyrics || lineWidth === 0) return 0;
-
-    // Calculate position as a percentage of the line width
-    const charCount = lyrics.length;
-    const positionPercentage = position / charCount;
-    return lineWidth * positionPercentage;
-  };
-
+export function LyricsLine({ lyrics, chords }: Props) {
   return (
     <View style={styles.container}>
-      <View style={styles.chordsContainer}>
-        {chords.map((chordPosition, index) => {
-          const x = getXPosition(chordPosition.position);
-
-          return (
-            <View
-              key={`chord-${index}`}
-              style={[
-                styles.chordWrapper,
-                {
-                  transform: [{ translateX: x }],
-                },
-              ]}
-            >
-              <ChordBox chord={chordPosition.chord} />
-            </View>
-          );
-        })}
+      <View style={styles.chordLine}>
+        {chords.map((chord, index) => (
+          <View
+            key={`chord-${index}`}
+            style={[
+              styles.chordWrapper,
+              { left: chord.position * 8 }, // Multiple by character width
+            ]}
+          >
+            <ChordBox chord={chord.chord} />
+          </View>
+        ))}
       </View>
-      <ThemedText style={styles.lyrics} onLayout={handleLayout}>
-        {lyrics}
-      </ThemedText>
+      <ThemedText style={styles.lyrics}>{lyrics}</ThemedText>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    marginVertical: 4,
+    marginVertical: 8,
   },
-  chordsContainer: {
+  chordLine: {
     height: 24,
     marginBottom: 4,
+    position: "relative",
   },
   chordWrapper: {
     position: "absolute",
-    alignItems: "center",
-    transform: [{ translateX: -50 }], // Center the chord box
+    top: 0,
   },
   lyrics: {
-    fontSize: 16,
-    lineHeight: 24,
+    fontSize: 14,
+    lineHeight: 20,
   },
 });
